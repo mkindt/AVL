8f9a91e9bbb99b3212256013a82663e22e860067
Michael Kindt <mkindt@unca.edu>
2012-11-16 09:25:40 -0500

    first comm(un)i(s)t

---
+++ Node.h
@@ -0,0 +1,22 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+template <typename T>
+class Node {
+ private:
+  T value;
+  Node<T>* leftChild;
+  Node<T>* rightChild;
+  int bal;
+
+ public:
+  Node<T>(T v);
+  T getValue();
+
+  Node<T>*& getLeftChild();
+  void setLeftChild(Node<T>& n);
+
+  Node<T>*& getRightChild();
+  void setRightChild(Node<T>& n);
+};
+#endif
